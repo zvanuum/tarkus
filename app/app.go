446b96760c921673b2cbbb6dc3b56329e6a531e0
Zach Van Uum <zachvanuum@gmail.com>
2017-11-12 19:42:46 -0700

    Added handler for registering other nodes in the network by simply using a string representation of a URL and a map as a set of known nodes.

--- app/app.go
+++ app/app.go
@@ -26,6 +26,8 @@ func (app *App) ServeHTTP(port int) {
 func InitializeRoutes(app *App, r *mux.Router) {
 	r.HandleFunc("/chain", api.GetChainHandler(app.Blockchain)).Methods("GET")
 	r.HandleFunc("/mine", api.GetMineHandler(app.Blockchain)).Methods("GET")
+	r.HandleFunc("/node/consensus", api.GetConsensusHandler(app.Blockchain)).Methods("GET")
 
 	r.HandleFunc("/transaction/new", api.PostNewTransactionHandler(app.Blockchain)).Methods("POST")
+	r.HandleFunc("/node/register", api.PostRegisterNodeHandler(app.Blockchain)).Methods("POST")
 }
